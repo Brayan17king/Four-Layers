8f1e1dbb413a023d02a55792fd34518a2097028b
Brayan17king <xbrayansneyder@gmail.com>
2023-10-25 12:25:23 -0500

    feat: :memo: Implementation of Services Web

--- WebApi/Services/IUserService.cs
+++ WebApi/Services/IUserService.cs
@@ -2,14 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Dtos;
 
 namespace WebApi.Services
 {
     public interface IUserService
     {
-       // Task<string> RegisterAsync(RegisterDto model);
-       // Task<DataUserDto> GetTokenAsync(LoginDto model);
-       // Task<string> AddRoleAsync(RegisterDto model);
-       // Task<DataUserDto> RefreshTokenAsync(string RefreshToken);
+        Task<string> RegisterAsync(RegisterDto model);
+        Task<DataUserDto> GetTokenAsync(LoginDto model);
+        Task<string> AddRoleAsync(AddRoleDto model);
+        Task<DataUserDto> RefreshTokenAsync(string RefreshToken);
     }
 }
